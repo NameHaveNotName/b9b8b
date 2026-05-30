65f4d600adb7e00f1f8943dbf29cb18d0b4bb909
康泽铭 <2627312372@qq.com>
2026-05-31 01:32:45 +0800

    style(badge): raise cost badge vertical position to align with button top edge

--- components/CostBadge.tsx
+++ components/CostBadge.tsx
@@ -7,7 +7,7 @@ export default function CostBadge({ cost = 0, showFree = true }: CostBadgeProps)
   if (cost === 0 && !showFree) return null
 
   return (
-    <span className="absolute -top-1.5 -right-2 text-[10px] text-gray-400 bg-transparent whitespace-nowrap">
+    <span className="absolute -top-3.5 -right-2 text-[10px] text-gray-400 bg-transparent whitespace-nowrap">
       {cost === 0 ? '免费' : `-${cost} 点`}
     </span>
   )
